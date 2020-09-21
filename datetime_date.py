ca19da86d6892e770ce03e4b34627180764842d8
tony-tomy <tony.tomy9@gmail.com>
2020-09-21 15:27:58 +0800

    day 1 : 21-09-2020

--- datetime_date.py
+++ datetime_date.py
@@ -1,3 +1,31 @@
-print("Hello World!")
+#!python3
 
-print("Changes in week_1 branch")
+from datetime import datetime
+from datetime import date
+
+datetime.today()
+
+today = datetime.today()
+
+print(type(today))
+
+todaydate = date.today()
+
+print(todaydate)
+
+print(type(todaydate))
+
+todaydate.month
+
+todaydate.year
+
+todaydate.day
+
+christmas = date(2020, 12, 25)
+
+christmas
+
+if christmas is not todaydate:
+    print("There are still "+ str((christmas-todaydate).days) + " days untill christmas")
+else:
+    print("Today is christmas")

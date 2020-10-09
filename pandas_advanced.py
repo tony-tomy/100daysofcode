bdc15941b91e25a8a95c86795d08edb910740348
tony-tomy <tony.tomy9@gmail.com>
2020-10-09 22:19:58 +0800

    day 19 : 09-10-2020

--- pandas_advanced.py
+++ pandas_advanced.py
@@ -45,6 +45,54 @@ df.to_html('example.html')
 
 df.rename(columns ={'Value' : 'Cost'}) # Rename a column
 
+# Reading data from URL
+
+import urllib.request
+import urllib.parse
+import re
+
+url = 'http://pythonprogramming.net'
+values = {'s':'basics',
+            'submit':'search'}
+data = urllib.parse.urlencode(values)
+data = data.encode('utf-8')
+req = urllib.request.Request(url, data)
+resp = urllib.request.urlopen(req)
+respData = resp.read()
+
+paragraphs = re.findall(r'<p>(.*?)</p>', str(respData))
+
+for eachP in paragraphs:
+   print(eachP)
 
 
+# Reading data from databases
 
+import sqlite3 
+
+#db connection
+
+try:
+   conn = sqlite3.connect()
+   print('Database connection was successful ')
+
+except Exception as e:
+   print('Error during connection ', str(e))
+
+#results = conn.execute("select * from company")
+
+#for row in results:
+#   print(row)
+
+#conn.close()
+
+# Read data from json
+
+EmployeeRecords = [{'EmployeeID':451621, 'EmployeeName':'Preeti Jain', 'DOJ':'30-Aug-2008'},
+{'EmployeeID':123621, 'EmployeeName':'Ashok Kumar', 'DOJ':'25-Sep-2016'},
+{'EmployeeID':451589, 'EmployeeName':'Johnty Rhodes', 'DOJ':'04-Nov-2016'}]
+
+import json
+emp_records_json_str = json.dumps(EmployeeRecords)
+df = pd.read_json(emp_records_json_str, orient='records', convert_dates=['DOJ'])
+print(df)

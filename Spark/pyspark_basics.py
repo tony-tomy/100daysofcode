38ebaad13c6d707dca86c3119b38a206caa8d680
tony-tomy <tony.tomy9@gmail.com>
2020-10-27 22:39:47 +0800

    day 37 : 27-10-2020

--- Spark/pyspark_basics.py
+++ Spark/pyspark_basics.py
@@ -353,6 +353,268 @@ To sort in descending order:
 
 df.filter((df.Club=='FC Barcelona') & (df.Nationality=='Spain')).orderBy('ID',ascending=False).show(5)
 
+Statistical and Mathematical Functions with DataFrames in Apache Spark
+
+Random Data Generation
+Random Data generation is useful when we want to test algorithms and to implement new ones.
+
+In Spark under sql.functions we have methods to generate random data. e.g., uniform (rand), and standard normal (randn).
+
+from pyspark.sql.functions import rand, randn
+df = sqlContext.range(0, 7)
+df.show()
+
+By using uniform distribution and normal distribution generate two more columns.
+
+df.select("id", rand(seed=10).alias("uniform"), randn(seed=27).alias("normal")).show()
+
+Summary and Descriptive Statistics
+The first operation to perform after importing data is to get some sense of what it looks like.
+
+The function describe returns a DataFrame containing information such as number of non-null entries (count), mean, standard deviation, and minimum and maximum value for each numerical column.
+
+df.describe('uniform', 'normal').show()
+
+Descriptive Statistics
+For a quick review of a column describe works fine.
+
+In the same way, we can also make use of some standard statistical functions also.
+
+from pyspark.sql.functions import mean, min, max
+df.select([mean('uniform'), min('uniform'), max('uniform')]).show()
+
+Sample Co-Variance and Correlation
+In statistics Co-Variance means how one random variable changes with respect to other.
+
+Positive value indicates a trend in increase when the other increases.
+
+Negative value indicates a trend in decrease when the other increases.
+
+from pyspark.sql.functions import rand
+df = sqlContext.range(0, 10).withColumn('rand1', rand(seed=10)).withColumn('rand2', rand(seed=27))
+df.stat.cov('rand1', 'rand2')
+
+Correlation provides the statistical dependence of two random variables.
+
+df.stat.corr('rand1', 'rand2')
+
+Two randomly generated columns have low correlation value.
+
+Cross Tabulation (Contingency Table)
+Cross Tabulation provides a frequency distribution table for a given set of variables.
+
+One of the powerful tool in statistics to observe the statistical independence of variables.
+
+# Create a DataFrame with two columns (name, item)
+names = ["Alice", "Bob", "Mike"]
+items = ["milk", "bread", "butter", "apples", "oranges"]
+df = sqlContext.createDataFrame([(names[i % 3], items[i % 5]) for i in range(100)], ["name", "item"])
+
+For applying the cross tabulation we can make use of the crosstab method.
+
+df.stat.crosstab("name", "item").show()
+
+Lab : Write multi column df to a text file.
+
+
+import pyspark.sql.functions as F
+
+df = sqlContext.createDataFrame([("foo", "bar"), ("baz", None)], 
+                            ('a', 'b'))
+
+def myConcat(*cols):
+    concat_columns = []
+    for c in cols[:-1]:
+        concat_columns.append(F.coalesce(c, F.lit("*")))
+        concat_columns.append(F.lit(" "))  
+    concat_columns.append(F.coalesce(cols[-1], F.lit("*")))
+    return F.concat(*concat_columns)
+
+df_text = df.withColumn("combined", myConcat(*df.columns)).select("combined")
+
+df_text.show()
+
+df_text.coalesce(1).write.format("text").option("header", "false").mode("append").save("output.txt")
+
+
+Spark SQL brings native support for SQL to Spark.
+
+Spark SQL blurs the lines between RDD's and relational tables.
+
+By integrating these powerful features Spark makes it easy for developers to use SQL commands for querying external data with complex analytics, all within in a single application.
+
+We can also pass SQL queries directly to any DataFrame.
+
+For that, we need to create a table from the DataFrame using the registerTempTable method.
+
+After that use sqlContext.sql() to pass the SQL queries.
+
+Apache Hive
+
+The Apache Hive data warehouse software allows reading, writing, and managing large datasets residing in distributed storage and queried using SQL syntax.
+
+Features of Apache Hive
+Apache Hive is built on top of Apache Hadoop.
+
+The below mentioned are the features of Apache Hive.
+
+Apache Hive is having tools to allow easy and quick access to data using SQL, thus enables data warehousing tasks such like extract/transform/load (ETL), reporting, and data analysis.
+Mechanisms for imposing structure on a variety of data formats.
+Access to files stored either directly in Apache HDFS or in other data storage systems such as Apache HBase.
+
+Features Of Apache Hive
+Query execution via Apache Tez,Apache Spark, or MapReduce.
+A procedural language with HPL-SQL.
+Sub-second query retrieval via Hive LLAP, Apache YARN and Apache Slider
+
+Apache Hive provides the standard SQL functionalities, which includes many of the later SQL:2003 and SQL:2011 features for analytics.
+
+We can extend Hive's SQL with the user code by using user-defined functions (UDFs), user-defined aggregates (UDAFs), and user-defined table functions (UDTFs).
+
+Hive comes with built-in connectors for comma and tab-separated values (CSV/TSV) text files, Apache Parquet,Apache ORC, and other formats.
+
+When working with Hive, one must instantiate SparkSession with Hive support, including connectivity to a persistent Hive metastore, support for Hive serdes, and Hive user-defined functions.
+
+How To Enable Hive Support
+from os.path import expanduser, join, abspath
+
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+
+#warehouse_location points to the default location for managed databases and tables
+
+warehouse_location = abspath('spark-warehouse')
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL Hive integration example") \
+    .config("spark.sql.warehouse.dir", warehouse_location) \
+    .enableHiveSupport() \
+    .getOrCreate()
+
+Creating Hive Table From Spark
+We can easily create a table in hive warehouse programmatically from Spark.
+
+The syntax for creating a table is as follows:
+
+spark.sql("CREATE TABLE IF NOT EXISTS table_name(column_name_1 DataType,column_name_2 DataType,......,column_name_n DataType) USING hive")
+
+To load a DataFrame into a table.
+
+df.write.insertInto("table name",overwrite = True)
+
+External tables are used to store data outside the hive.
+Data needs to remain in the underlying location even after the user drop the table.
+
+Handling External Hive Tables From Apache Spark
+First, create an external table in the hive by specifying the location.
+
+One can create an external table in the hive by running the following query on hive shell.
+
+hive> create external table table_name(column_name1 DataType,column_name2 DataType,......,column_name_n DataType) STORED AS Parquet location ' path of external table';
+
+The table is created in Parquet schema.
+The table is saved in the hdfs directory.
+
+Loading Data From Spark To The Hive Table
+We can load data to hive table from the DataFrame.
+
+For doing the same schema of both hive table and the DataFrame should be equal.
+
+Let us take a sample CSV file.
+
+We can read the csv the file by making use of spark csv reader.
+
+df = spark.read.csv("path-of-file", inferSchema = True, header = True)
+The schema of the DataFrame will be same as the schema of the CSV file itself.
+
+Data Loading To External Table
+For loading the data we have to save the dataframe in external hive table location.
+
+df.write.mode('overwrite').format("format").save("location")
+Since our hive external table is in parquet format in place of format we have to mention 'parquet'.
+
+The location should be same as the hive external table location in hdfs directory.
+
+If the schema is matching then data will load automatically to the hive table.
+
+HBASE 
+
+HBase is a distributed column-oriented data store built on top of HDFS.
+
+HBase is an Apache open source project whose goal is to provide storage for the Hadoop Distributed Computing.
+
+Data is logically organized into tables, rows and columns.
+
+HBase features compression, in-memory operation, and Bloom filters on a per-column basis as outlined in the original Bigtable paper.
+Tables in HBase can serve as the input and output for Map Reduce jobs run in Hadoop, and may be accessed through the Java API but also through REST, Avro or Thrift gateway APIs.
+It is a column-oriented key-value data store and has been idolized widely because of its lineage with Hadoop and HDFS.
+HBase runs on top of HDFS and is well-suited for faster read and write operations on large datasets with high throughput and low input/output latency.
+
+How To Connect Spark and HBase
+To connect we require hdfs,Spark and HBase installed in the local machine.
+Make sure that your versions are matching with each other.
+Copy all the HBase jar files to the Spark lib folder.
+Once done set the SPARK_CLASSPATH in spark-env.sh with lib.
+
+Real time pipeline using HBASE
+
+It has 4 main stages which includes:
+
+Transformation
+Cleaning
+Validation
+Writing of the data received from the various sources
+
+Data Transformation
+
+This is an entry point for the streaming application.
+Here the operations related to normalization of data are performed.
+Transformation of data can be performed by using built-in functions like map, filter, foreachRDD etc.
+
+Data Cleaning
+
+During preprocessing cleaning is very important.
+In this stage, we can use custom built libraries for cleaning the data
+
+Data Validation
+
+In this stage, we can validate the data with respect to some standard validations such as length, patterns and so on.
+
+Writing
+
+At last, data passed from the previous three stages is passed on to the writing application which simply writes this final set of data to HBase for further data analysis.
+
+Spark In Real World
+Uber – the online taxi company is an apt example for Spark. They are gathering terabytes of event data from its various users.
+
+Uses Kafka, Spark Streaming, and HDFS, to build a continuous ETL pipeline.
+Convert raw unstructured data into structured data as it is collected.
+Uses it further complex analytics and optimization of operations.
+
+Pinterest – Uses a Spark ETL pipeline
+
+Leverages Spark Streaming to gain immediate insight into how users all over the world are engaging with Pins in real time.
+Can make more relevant recommendations as people navigate the site.
+Recommends related Pins.
+Determine which products to buy, or destinations to visit.
+
+Conviva – 4 million video feeds per month.
+
+Conviva is using Spark for reducing the customer churn by managing live video traffic and optimizing video streams.
+They maintain a consistently smooth high-quality viewing experience.
+
+Capital One – makes use of Spark and data science algorithms for a better understanding of its customers.
+
+Developing the next generation of financial products and services.
+Find attributes and patterns of increased probability for fraud.
+
+Netflix – Movie recommendation engine from user data.
+
+User data is also used for content creation.
+
+
+
 
 
 

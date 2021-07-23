acef4b757faf081237e5f681122665894cddc09a
tony-tomy <tony.tomy9@gmail.com>
2021-07-23 17:35:02 +0800

    working with csv

--- Data Engineering/working_with_csv.py
+++ Data Engineering/working_with_csv.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv('taxi.csv.bz2')
+df = pd.read_csv('Data Engineering/taxi.csv.bz2')
 print(df.dtypes)
+
+time_cols = ['tpep_pickup_datetime','tpep_dropoff_datetime']
+
+df1 = pd.read_csv('Data Engineering/taxi.csv.bz2', parse_dates= time_cols)
+print(df1.dtypes)
